865626d26a061b6afd9247fb737d6191050334f4
tibinthomas <tibinthomas@outlook.com>
2022-09-16 12:17:50 +0530

    add new test cases

--- test-ng-project/angular-unused-css-finder-test/src/app/app.module.ts
+++ test-ng-project/angular-unused-css-finder-test/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +13,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  
 })
 export class AppModule { }
