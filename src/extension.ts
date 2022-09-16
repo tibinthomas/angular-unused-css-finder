78abcc7ae27cd41d686f803eb7d2bdc0afed519f
tibinthomas <tibinthomas@outlook.com>
2022-09-16 11:54:55 +0530

    init test project

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "angular-unused-css-finder" is now active!');
 
+	
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
