1c4f22c01fc8dbfb24092f51204396f232617934
tibinthomas <tibinthomas@outlook.com>
2022-09-16 15:56:13 +0530

    getcss  class array from the selected css file's linked htm file

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "angular-unused-css-finder" is now active!');
-
 	
+	
+	
+	context.subscriptions.push(vscode.commands.registerCommand('angular-unused-css-finder.scan-for-unused', () => {
+		const activeCssFileText = vscode.window.activeTextEditor?.document.getText();
+		const activeCssFileUri = vscode.window.activeTextEditor?.document.uri;
+		const linkedHtmlFileUri = activeCssFileUri?.fsPath.replace('.component.css', '.component.html');
+		let linkedHtmlFileText = '';
+		let arr;
+		try {
+			if (linkedHtmlFileUri) {
+				linkedHtmlFileText = fs.readFileSync(linkedHtmlFileUri, { encoding: 'utf-8' })
+				arr = linkedHtmlFileText.match(/(?:class=")[^"]*"/g)?.map(x => x.replace('class="', "").replace("\"", "").split(" ")).flat();
+			} else {
+				console.log(`linkedHtmlFileText - Can't be empty`);
+			}
+		} catch {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('angular-unused-css-finder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from angular-unused-css-finder!');
-	});
+		}
+		
+		if (arr?.some((x: string) => activeCssFileText?.includes(x))) {
+			//
+		}
+		
+	}));
 
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
