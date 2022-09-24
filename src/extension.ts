2560b11a064d66bb4915b55a460dd3dd7d2a29f5
tibinthomas <tibinthomas@carestack.com>
2022-09-24 11:27:17 +0530

    handle non emulated encapsulation

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as fs from "fs";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	
-	
-	
-	context.subscriptions.push(vscode.commands.registerCommand('angular-unused-css-finder.scan-for-unused', () => {
-		const activeCssFileText = vscode.window.activeTextEditor?.document.getText();
-		const activeCssFileUri = vscode.window.activeTextEditor?.document.uri;
-		const linkedHtmlFileUri = activeCssFileUri?.fsPath.replace('.component.css', '.component.html');
-		let linkedHtmlFileText = '';
-		let arr;
-		try {
-			if (linkedHtmlFileUri) {
-				linkedHtmlFileText = fs.readFileSync(linkedHtmlFileUri, { encoding: 'utf-8' })
-				arr = linkedHtmlFileText.match(/(?:class=")[^"]*"/g)?.map(x => x.replace('class="', "").replace("\"", "").split(" ")).flat();
-			} else {
-				console.log(`linkedHtmlFileText - Can't be empty`);
-			}
-		} catch {
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "angular-unused-css-finder.scan-for-unused",
+      () => {
+        if (!vscode.window.activeTextEditor) {
+          console.log("No active text editor");
+          return;
+        }
+        const activeEditor = vscode.window.activeTextEditor;
 
-		}
-		
-		if (arr?.some((x: string) => activeCssFileText?.includes(x))) {
-			//
-		}
-		
-	}));
+        const activeCssFileText = activeEditor?.document.getText();
+        const activeCssFileUri = activeEditor?.document.uri;
+        const linkedHtmlFileUri = activeCssFileUri?.fsPath.replace(
+          ".component.css",
+          ".component.html"
+        );
+        const linkedTSFileUri = activeCssFileUri?.fsPath.replace(
+          ".component.css",
+          ".component.ts"
+        );
+        let linkedHtmlFileText = "";
+        let linkedTSFileText = "";
+        let classesInLinkedHtmlFile;
+        let isViewEncapsulationNone = false;
+        try {
+          if (linkedHtmlFileUri) {
+            linkedHtmlFileText = fs.readFileSync(linkedHtmlFileUri, {
+              encoding: "utf-8",
+            });
+            classesInLinkedHtmlFile = linkedHtmlFileText
+              .match(/(?:class=")[^"]*"/g)
+              ?.map((x) => x.replace('class="', "").replace('"', "").split(" "))
+              .flat();
+          } else {
+            console.log(`linkedHtmlFileText - Can't be empty`);
+          }
+        } catch {}
 
+        try {
+          if (linkedTSFileUri) {
+            linkedTSFileText = fs.readFileSync(linkedTSFileUri, {
+              encoding: "utf-8",
+            });
+            isViewEncapsulationNone = linkedTSFileText.includes(
+              "ViewEncapsulation.None"
+            );
+          } else {
+            console.log(`linkedTSFileText - Can't be empty`);
+          }
+        } catch {}
+
+        if (
+          isViewEncapsulationNone &&
+          classesInLinkedHtmlFile?.some((x: string) =>
+            activeCssFileText?.includes(x)
+          )
+        ) {
+          //
+        }
+      }
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
